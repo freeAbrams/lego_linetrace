39eca0260d43d33443d1fc62871f745016a555ae
dong <iamsc2_master@outlook.com>
2023-06-20 07:43:18 +0900

    fixed

--- app.c
+++ app.c
@@ -43,6 +43,7 @@ void main_task(intptr_t unused)
     /* タスクを開始する */ 
     ev3_sta_cyc(LOG_CYC);
     act_tsk(TRACE_TASK);
+    act_tsk(JUNCTION_TASK);  // junction_taskがアクティブになり、待機状態になる
 }
 
 void log_task(intptr_t unused)
@@ -69,29 +70,29 @@ void trace_task(intptr_t unused)
     {
         while(!color_flag)
         {
-        /* 左右のセンサーの反射値を取得 */
-        int l_reflect = ev3_color_sensor_get_reflect(L_SENSOR);
-        int r_reflect = ev3_color_sensor_get_reflect(R_SENSOR);
+            /* 左右のセンサーの反射値を取得 */
+            int l_reflect = ev3_color_sensor_get_reflect(L_SENSOR);
+            int r_reflect = ev3_color_sensor_get_reflect(R_SENSOR);
 
-        /* モーターの角度差を計算 */
-        l_motor_angle = ev3_motor_get_counts(L_MOTOR);
-        r_motor_angle = ev3_motor_get_counts(R_MOTOR);
-        diff = l_motor_angle - r_motor_angle;
+            /* モーターの角度差を計算 */
+            l_motor_angle = ev3_motor_get_counts(L_MOTOR);
+            r_motor_angle = ev3_motor_get_counts(R_MOTOR);
+            diff = l_motor_angle - r_motor_angle;
 
-        /* 左右の反射値に応じてロボットを制御 */
-        int l_speed = speed - (l_reflect - target);
-        int r_speed = speed - (r_reflect - target);
-        ev3_motor_set_power(L_MOTOR, l_speed);
-        ev3_motor_set_power(R_MOTOR, r_speed);
+            /* 左右の反射値に応じてロボットを制御 */
+            int l_speed = speed - (l_reflect - target);
+            int r_speed = speed - (r_reflect - target);
+            ev3_motor_set_power(L_MOTOR, l_speed);
+            ev3_motor_set_power(R_MOTOR, r_speed);
 
-        /* 色センサーが青、赤、黄を検出するまでループ */
-        if(ev3_color_sensor_get_color(L_SENSOR) == COLOR_BLUE || ev3_color_sensor_get_color(L_SENSOR) == COLOR_RED || ev3_color_sensor_get_color(L_SENSOR) == COLOR_YELLOW || ev3_color_sensor_get_color(R_SENSOR) == COLOR_BLUE || ev3_color_sensor_get_color(R_SENSOR) == COLOR_RED || ev3_color_sensor_get_color(R_SENSOR) == COLOR_YELLOW)
-        {
-            ev3_motor_reset_counts(L_MOTOR);
-            ev3_motor_reset_counts(R_MOTOR);
-            color_flag = true;
-            break;
-        }
+            /* 色センサーが青、赤、黄を検出するまでループ */
+            if(ev3_color_sensor_get_color(L_SENSOR) == COLOR_BLUE || ev3_color_sensor_get_color(L_SENSOR) == COLOR_RED || ev3_color_sensor_get_color(L_SENSOR) == COLOR_YELLOW || ev3_color_sensor_get_color(R_SENSOR) == COLOR_BLUE || ev3_color_sensor_get_color(R_SENSOR) == COLOR_RED || ev3_color_sensor_get_color(R_SENSOR) == COLOR_YELLOW)
+            {
+                ev3_motor_reset_counts(L_MOTOR);
+                ev3_motor_reset_counts(R_MOTOR);
+                color_flag = true;
+                break;
+            }
         }
     }
 }
@@ -110,52 +111,53 @@ void junction_task(intptr_t unused)
     {
         while(color_flag)
         {
-        /* モーターの角度を計算 */
-        l_motor_angle = ev3_motor_get_counts(L_MOTOR);
-        r_motor_angle = ev3_motor_get_counts(R_MOTOR);
-        diff = l_motor_angle - r_motor_angle;
+            /* モーターの角度を計算 */
+            l_motor_angle = ev3_motor_get_counts(L_MOTOR);
+            r_motor_angle = ev3_motor_get_counts(R_MOTOR);
+            diff = l_motor_angle - r_motor_angle;
 
-        /* カラーセンサーで色を検出 */
-        color = ev3_color_sensor_get_color(L_SENSOR);
+            /* カラーセンサーで色を検出 */
+            color = ev3_color_sensor_get_color(L_SENSOR);
 
-        /* カラーに応じて動作を変更 */
-        switch (color) 
+            /* カラーに応じて動作を変更 */
+            switch (color) 
+            {
+                case COLOR_YELLOW:
+                    diff_yellow = diff;
+                    ev3_motor_set_power(L_MOTOR, rotate_speed);
+                    ev3_motor_set_power(R_MOTOR, -rotate_speed);
+                    break;
+                case COLOR_RED:
+                    diff_red = diff;
+                    ev3_motor_set_power(L_MOTOR, rotate_speed);
+                    ev3_motor_set_power(R_MOTOR, -rotate_speed);
+                    break;
+                case COLOR_GREEN:
+                    diff_green = diff;
+                    found_green = true;
+                    color_flag = false; // 緑を見つけたので、color_flagをリセット
+                    break;
+                default:
+                    ev3_motor_set_power(L_MOTOR, rotate_speed);
+                    ev3_motor_set_power(R_MOTOR, -rotate_speed);
+                    break;
+            }
+
+            /* 少し待つ */
+            tslp_tsk(10);
+        }
+
+        /* 緑に向かって前進 */
+        if(found_green)
         {
-            case COLOR_YELLOW:
-                color_flag = true;
-                diff_yellow = diff;
-                ev3_motor_set_power(L_MOTOR, rotate_speed);
-                ev3_motor_set_power(R_MOTOR, -rotate_speed);
-                break;
-            case COLOR_RED:
-                color_flag = true;
-                diff_red = diff;
-                ev3_motor_set_power(L_MOTOR, rotate_speed);
-                ev3_motor_set_power(R_MOTOR, -rotate_speed);
-                break;
-            case COLOR_GREEN:
-                diff_green = diff;
-                found_green = true;
-                color_flag = false;
-                break;
-            default:
-                ev3_motor_set_power(L_MOTOR, rotate_speed);
-                ev3_motor_set_power(R_MOTOR, -rotate_speed);
-                break;
+            int target_diff = diff_green;
+            while(abs(ev3_motor_get_counts(L_MOTOR) - ev3_motor_get_counts(R_MOTOR)) < abs(target_diff))
+            {
+                ev3_motor_set_power(L_MOTOR, speed);
+                ev3_motor_set_power(R_MOTOR, speed);
+                tslp_tsk(10);
+            }
+            found_green = false; // 緑の分岐点を過ぎたらフラグをリセット
         }
-
-        /* 少し待つ */
-        tslp_tsk(10);
-        }
-    }
-    
-
-    /* 緑に向かって前進 */
-    int target_diff = diff_green;
-    while(abs(ev3_motor_get_counts(L_MOTOR) - ev3_motor_get_counts(R_MOTOR)) < abs(target_diff))
-    {
-        ev3_motor_set_power(L_MOTOR, speed);
-        ev3_motor_set_power(R_MOTOR, speed);
-        tslp_tsk(10);
     }
 }
