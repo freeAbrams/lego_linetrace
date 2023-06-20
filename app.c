94b6b63abf2f01102822491ea5db46263cfffb57
haoyangdong <iamsc2_master@outlook.com>
2023-06-20 17:23:26 +0900

    rebuild

--- app.c
+++ app.c
@@ -1,23 +1,18 @@
-/* ヘッダーファイルの読み込み */
 #include "ev3api.h"
 #include "app.h"
-
 #if defined(BUILD_MODULE)
 #include "module_cfg.h"
 #else
 #include "kernel_cfg.h"
 #endif
 
-/* ポートの設定 */
 #define L_MOTOR EV3_PORT_C /* 左モーターポートの設定 */
 #define R_MOTOR EV3_PORT_B /* 右モーターポートの設定 */
 #define L_SENSOR EV3_PORT_3 /* 左センサーポートの設定 */
 #define R_SENSOR EV3_PORT_2 /* 右センサーポートの設定 */
-#define LOG_FILENAME "log.txt" /* ログファイルの名前 */
 
-/* 定数の設定 */
-const int target = 30; /* 黒の線上を走行するための目標反射値 */
-const int speed = 50; /* モーターのスピード */
+const int target = 90; /* 黒の線上を走行するための目標反射値 */
+const int speed = 15; /* モーターのスピード */
 const int rotate_speed = 10; /* ロボットの旋回スピード */
 const int motor_angle_threshold = 20; /* モーターの角度閾値 */
 const int color_threshold = 60; /* 色の閾値 */
@@ -26,142 +21,95 @@ const int color_threshold = 60; /* 色の閾値 */
 FILE *log_file = NULL; /* ログファイルのポインタ */
 SYSTIM start_time; /* 開始時間 */
 
-int found_green = 0;
+//int found_green = 0;
 int color_flag = 0; /* 特定の色を検出したかどうかのフラグ */
 
+int l_reflect = 0; /* 左センサーの反射値 */
+int r_reflect = 0; /* 右センサーの反射値 */
+int l_motor_angle = 0;
+int r_motor_angle = 0;
+int l_speed = 0;
+int r_speed = 0;
 
-void log_task(intptr_t unused)
+void log_task(intptr_t exinf)
 {
-    if(log_file != NULL)
+    while (1)
     {
         SYSTIM now_time;
         get_tim(&now_time);
-        fprintf(log_file, "%ld, %ld, %ld, %d, %d, %d, %d\n", now_time - start_time,
-                ev3_motor_get_counts(L_MOTOR), ev3_motor_get_counts(R_MOTOR),
-                ev3_motor_get_power(L_MOTOR), ev3_motor_get_power(R_MOTOR),
-                ev3_color_sensor_get_reflect(L_SENSOR), ev3_color_sensor_get_reflect(R_SENSOR));
+        fprintf(log_file, "TIME = %d, L_SENSOR = %d, R_SENSOR = %d, L_MOTOR = %d, R_MOTOR = %d\n", now_time - start_time, l_reflect, r_reflect, l_speed, r_speed);
+        tslp_tsk(500);
     }
 }
 
-void trace_task(intptr_t unused)
+void cyclic_task(intptr_t exinf) 
 {
-    int l_motor_angle = 0;
-    int r_motor_angle = 0;
-    int diff = 0;
-    colorid_t color;
-    ev3_motor_set_power(L_MOTOR, 30);
-            ev3_motor_set_power(R_MOTOR, 30);
-    while (1)
+    while (color_flag == 0)
     {
-        while(!color_flag)
+        /* 左右のセンサーの反射値を取得 */
+        l_reflect = ev3_color_sensor_get_reflect(L_SENSOR);
+        r_reflect = ev3_color_sensor_get_reflect(R_SENSOR);
+        int reflect_diff = l_reflect - r_reflect;
+        
+
+        /* モーターの角度差を計算 */
+        l_motor_angle = ev3_motor_get_counts(L_MOTOR);
+        r_motor_angle = ev3_motor_get_counts(R_MOTOR);
+        int diff = l_motor_angle - r_motor_angle;
+
+        /* 左右の反射値に応じてロボットを制御 */
+        l_speed = speed + (l_reflect - r_reflect)/4;
+        r_speed = speed + (r_reflect - l_reflect)/4;
+        
+        ev3_motor_set_power(L_MOTOR, l_speed);
+        ev3_motor_set_power(R_MOTOR, r_speed);
+        //ev3_motor_steer(L_MOTOR, R_MOTOR, speed, reflect_diff);
+
+        /* 色センサーが青、赤、黄を検出するまでループ */
+        if(ev3_color_sensor_get_color(L_SENSOR) == COLOR_BLUE || ev3_color_sensor_get_color(L_SENSOR) == COLOR_RED || ev3_color_sensor_get_color(L_SENSOR) == COLOR_YELLOW || ev3_color_sensor_get_color(R_SENSOR) == COLOR_BLUE || ev3_color_sensor_get_color(R_SENSOR) == COLOR_RED || ev3_color_sensor_get_color(R_SENSOR) == COLOR_YELLOW)
         {
-            /* 左右のセンサーの反射値を取得 */
-            int l_reflect = ev3_color_sensor_get_reflect(L_SENSOR);
-            int r_reflect = ev3_color_sensor_get_reflect(R_SENSOR);
-
-            /* モーターの角度差を計算 */
-            l_motor_angle = ev3_motor_get_counts(L_MOTOR);
-            r_motor_angle = ev3_motor_get_counts(R_MOTOR);
-            diff = l_motor_angle - r_motor_angle;
-
-            /* 左右の反射値に応じてロボットを制御 */
-            int l_speed = speed - (l_reflect - target);
-            int r_speed = speed - (r_reflect - target);
-            
-            //ev3_motor_set_power(L_MOTOR, l_speed);
-            //ev3_motor_set_power(R_MOTOR, r_speed);
-
-            /* 色センサーが青、赤、黄を検出するまでループ */
-            if(ev3_color_sensor_get_color(L_SENSOR) == COLOR_BLUE || ev3_color_sensor_get_color(L_SENSOR) == COLOR_RED || ev3_color_sensor_get_color(L_SENSOR) == COLOR_YELLOW || ev3_color_sensor_get_color(R_SENSOR) == COLOR_BLUE || ev3_color_sensor_get_color(R_SENSOR) == COLOR_RED || ev3_color_sensor_get_color(R_SENSOR) == COLOR_YELLOW)
-            {
-                ev3_motor_reset_counts(L_MOTOR);
-                ev3_motor_reset_counts(R_MOTOR);
-                color_flag = 1;
-                break;
-            }
+            ev3_motor_reset_counts(L_MOTOR);
+            ev3_motor_reset_counts(R_MOTOR);
+            color_flag = 1;
+            break;
         }
     }
-}
-
-void junction_task(intptr_t unused)
-{
-    int l_motor_angle = 0;
-    int r_motor_angle = 0;
-    int diff = 0;
-    int diff_yellow = 0;
-    int diff_red = 0;
-    int diff_green = 0;
-    colorid_t color;
-    
-    while (1)
+    while (color_flag ==1)
     {
-        while(color_flag)
+        ev3_motor_steer(L_MOTOR, R_MOTOR, 10, 20);
+        tslp_tsk(500);
+        ev3_motor_steer(L_MOTOR, R_MOTOR, 10, -20);
+        tslp_tsk(500);
+        if (ev3_color_sensor_get_color(L_SENSOR) == COLOR_GREEN ||ev3_color_sensor_get_color(R_SENSOR) == COLOR_GREEN)
         {
-            /* モーターの角度を計算 */
-            l_motor_angle = ev3_motor_get_counts(L_MOTOR);
-            r_motor_angle = ev3_motor_get_counts(R_MOTOR);
-            diff = l_motor_angle - r_motor_angle;
-
-            /* カラーセンサーで色を検出 */
-            color = ev3_color_sensor_get_color(L_SENSOR);
-
-            /* カラーに応じて動作を変更 */
-            switch (color) 
-            {
-                case COLOR_YELLOW:
-                    diff_yellow = diff;
-                    ev3_motor_set_power(L_MOTOR, rotate_speed);
-                    ev3_motor_set_power(R_MOTOR, -rotate_speed);
-                    break;
-                case COLOR_RED:
-                    diff_red = diff;
-                    ev3_motor_set_power(L_MOTOR, rotate_speed);
-                    ev3_motor_set_power(R_MOTOR, -rotate_speed);
-                    break;
-                case COLOR_GREEN:
-                    diff_green = diff;
-                    found_green = 1;
-                    color_flag = 0; // 緑を見つけたので、color_flagをリセット
-                    break;
-                default:
-                    ev3_motor_set_power(L_MOTOR, rotate_speed);
-                    ev3_motor_set_power(R_MOTOR, -rotate_speed);
-                    break;
-            }
-
-            /* 少し待つ */
-            tslp_tsk(10);
+            color_flag = 0;
+            break;
         }
-
-        /* 緑に向かって前進 */
-        if(found_green)
+        if (ev3_color_sensor_get_color(L_SENSOR) == COLOR_YELLOW ||ev3_color_sensor_get_color(R_SENSOR) == COLOR_YELLOW)
         {
-            int target_diff = diff_green;
-            while(abs(ev3_motor_get_counts(L_MOTOR) - ev3_motor_get_counts(R_MOTOR)) < abs(target_diff))
-            {
-                ev3_motor_set_power(L_MOTOR, speed);
-                ev3_motor_set_power(R_MOTOR, speed);
-                tslp_tsk(10);
-            }
-            found_green = 0; // 緑の分岐点を過ぎたらフラグをリセット
+            ev3_motor_steer(L_MOTOR, R_MOTOR, 10, 60);
+            tslp_tsk(500);
+            break;
         }
-    }
+        if (ev3_color_sensor_get_color(L_SENSOR) == COLOR_RED ||ev3_color_sensor_get_color(R_SENSOR) == COLOR_RED)
+        {
+            ev3_motor_steer(L_MOTOR, R_MOTOR, 10, 60);
+            tslp_tsk(500);
+            break;
+        }
+    }    
 }
 
-void main_task(intptr_t unused) 
-{ 
-    /* モーターとセンサーの設定 */
-    ev3_sensor_config(L_SENSOR, COLOR_SENSOR);
-    ev3_sensor_config(R_SENSOR, COLOR_SENSOR);
+void main_task(intptr_t unused){
+    //モーターの設定
     ev3_motor_config(L_MOTOR, LARGE_MOTOR);
     ev3_motor_config(R_MOTOR, LARGE_MOTOR);
-    
-    /* ログファイルを開く */
-    log_file = fopen(LOG_FILENAME, "w");
-    get_tim(&start_time);
-
-    /* タスクを開始する */ 
-    act_tsk(trace_task);
-    ev3_sta_cyc(log_task);
-    act_tsk(junction_task);  // junction_taskがアクティブになり、待機状態になる
+    //センサの設定
+    ev3_sensor_config(R_SENSOR, COLOR_SENSOR);
+    ev3_sensor_config(L_SENSOR, COLOR_SENSOR);
+    //タスクを開始する
+    ev3_sta_cyc(CYCHDR1);
+    act_tsk((intptr_t)log_task);
+    ev3_sta_cyc(CYCHDR2);
+    act_tsk((intptr_t)cyclic_task);
 }
